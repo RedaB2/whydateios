0125cd5f97ffce4bdd540de887bb8cc4bbaac0aa
Reda <wayzfut@gmail.com>
2024-08-15 00:23:53 -0500

    Initial Commit

---
+++ whydate/whydateApp.swift
@@ -0,0 +1,17 @@
+//
+//  whydateApp.swift
+//  whydate
+//
+//  Created by Reda Boutayeb on 8/15/24.
+//
+
+import SwiftUI
+
+@main
+struct whydateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
