c1ffb8e1e900c21989bbffba5e8b546903062ba1
RedaB <wayzfut@gmail.com>
2024-08-31 19:02:03 -0700

    implemented questionnaire and reauthentification for account deletion

---
+++ whydate/ReauthenticatePromptView.swift
@@ -0,0 +1,18 @@
+//
+//  ReauthenticatePromptView.swift
+//  whydate
+//
+//  Created by Reda Boutayeb on 8/31/24.
+//
+
+import SwiftUI
+
+struct ReauthenticatePromptView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReauthenticatePromptView()
+}
